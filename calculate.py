b76cf5150b170b722c25351e8900beb86cf0d6c4
Swapnil <swapnilpathak1997@gmail.com>
2022-11-10 11:06:08 +0530

    This is Calculator code

---
+++ calculate.py
@@ -0,0 +1,8 @@
+class Calculator:
+
+    def __init__(self):
+        expression = input("Enter the expression here: ") #Enter Your Expression here
+        #To print the Output
+        print(eval(expression))
+
+Calculator()
